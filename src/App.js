e99433eeaefba255be4ab1a344248f16ae032198
Dida Patria <55367940+didapatria@users.noreply.github.com>
2022-06-20 00:25:12 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-/* eslint-disable prettier/prettier */
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Home from "./pages/Home";
-// import LoginPage from "./pages/auth/Login";
-// import RegisterPage from "./pages/auth/Register";
+import Home from "./pages/Home";
+import LoginPage from "./pages/auth/Login";
+import RegisterPage from "./pages/auth/Register";
 import AddProductPage from "./pages/AddProduct";
-// import NotFound from "./pages/NotFound";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
-        {/* <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} /> */}
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/addproduct" element={<AddProductPage />} />
-        {/* <Route path="*" element={<NotFound />} /> */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
