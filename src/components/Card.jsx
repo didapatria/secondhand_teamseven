475843ebfd210c03c7e9ad38231adccae6064041
Dida Patria <55367940+didapatria@users.noreply.github.com>
2022-07-22 20:54:21 +0700

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,27 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function Card(props) {
   const products = props.data;
+
+  const [image, setImage] = useState([]);
+
+  const getImage = async () => {
+    try {
+      const productId = 1;
+      // const productId = products[i].id;
+      const dataImage = await axios.get(
+        `https://apisecondhand.herokuapp.com/api/product/${productId}/images`,
+      );
+      setImage(await dataImage.data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getImage();
+  }, []);
 
   const currencyFormatter = (value) => {
     const formattedAmount = Math.trunc(value)
@@ -18,7 +40,7 @@ export default function Card(props) {
       {products.map((key, i) => (
         <div className="h-60 space-y-2 rounded bg-white p-2 shadow" key={key}>
           <img
-            src="../assets/images/product.jpg"
+            src={image[0].imageUrl}
             alt=""
             className="h-3/5 w-full rounded object-cover"
           />
